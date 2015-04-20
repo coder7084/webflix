7a8a1aacb7e57cf7b547819a9d587f2de591b5c3
Teena <teena7084@gmail.com>
2015-04-19 21:25:41 -0500

    add general.js

--- js/general.js
+++ js/general.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {  
- $('input[name=id]').val('124679');  
- //$('input[name=id]').attr('disabled', 'disabled');
- 
- 
- 
+   var max = 9999999999;
+   var min = 1000000000;
+   var id =  Math.round(Math.floor(Math.random() * (max - min)) / 10) ;
+   $('input[name=id]').val(id);  
+   
 
+$('#submitbutton').click( function(){    
+   $('.account_id').removeAttr('disabled');
+   
+ });
+ 
 }); //end of doc.ready
 
-$('#submitbutton').click( function(){
-   //$('input[name=id]').removeClass('disabled');
- });
